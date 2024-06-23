f5e30a39d7187af1a69056e9e47248329bd8a2d4
Przemek <przemyslaw.gorecki93@gmail.com>
2024-06-23 16:32:41 +0200

    Use useSearchParams

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
-import { TextField } from "@mui/material";
+import { IconButton, TextField } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
 import { debounce } from "lodash";
 import { createData } from "../utils/createData";
 import { getDrugsList } from "../services/drugs";
-import useQueryParams from "../hooks/useQueryParams";
 import DrugsListTable from "../components/DrugsListTable";
 import { Result, Row } from "../models";
+import ClearIcon from "@mui/icons-material/Clear";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
   const [drugs, setDrugs] = useState<Row[]>([]);
@@ -13,10 +14,10 @@ const Home = () => {
   const [totalResults, setTotalResults] = useState<number>(0);
   const [error, setError] = useState<string | null>(null);
 
-  const { setQueryParam, getQueryParam } = useQueryParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const searchQuery = getQueryParam("search") || "";
-  const pageQuery = Number(getQueryParam("page")) || 1;
+  const searchQuery = searchParams.get("search") || "";
+  const pageQuery = Number(searchParams.get("page")) || 1;
 
   const debouncedGetDrugsList = useCallback(
     debounce(async (searchTerm: string, page: number) => {
@@ -59,20 +60,28 @@ const Home = () => {
     _event: React.MouseEvent<HTMLButtonElement> | null,
     newPage: number
   ) => {
-    setQueryParam("page", String(newPage + 1));
+    setSearchParams((prevSearchParams) => ({
+      page: String(newPage + 1),
+      search: prevSearchParams.get("search") || "",
+    }));
   };
 
   const handleChangeRowsPerPage = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     setRowsPerPage(parseInt(event.target.value, 10));
-    setQueryParam("page", "1");
+
+    setSearchParams((prevSearchParams) => ({
+      page: "1",
+      search: prevSearchParams.get("search") || "",
+    }));
+  };
+
+  const handleClearSearch = () => {
+    setSearchParams({ search: "", page: "1" });
   };
 
   useEffect(() => {
-    if (!searchQuery) {
-      setQueryParam("page", "1");
-    }
     debouncedGetDrugsList(searchQuery, pageQuery);
   }, [debouncedGetDrugsList, searchQuery, pageQuery]);
 
@@ -82,10 +91,21 @@ const Home = () => {
         <TextField
           id="outlined-basic"
           label="Search medication"
+          InputProps={{
+            endAdornment: searchQuery && (
+              <IconButton onClick={handleClearSearch}>
+                <ClearIcon />
+              </IconButton>
+            ),
+          }}
           variant="outlined"
-          value={getQueryParam("search") || ""}
-          onChange={(e) => setQueryParam("search", e.target.value)}
-        />
+          value={searchQuery}
+          onChange={(e) =>
+            setSearchParams({ search: e.target.value, page: "1" })
+          }
+        >
+          clear
+        </TextField>
       </div>
 
       <DrugsListTable

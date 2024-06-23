aeecbdb683845467416b93be5a700df956104f4c
Przemek <przemyslaw.gorecki93@gmail.com>
2024-06-23 20:57:38 +0200

    Position pagination

--- src/components/DrugsListTable.tsx
+++ src/components/DrugsListTable.tsx
@@ -6,7 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { TableFooter, TablePagination } from "@mui/material";
+import { TablePagination } from "@mui/material";
 import TablePaginationControls from "./TablePaginationControls";
 import { Row } from "../models";
 import ErrorFallback from "./ErrorFallback";
@@ -60,66 +60,66 @@ const DrugsListTable = ({
   error,
 }: DrugsListTableProps) => {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Brand Name</StyledTableCell>
-            <StyledTableCell align="right">Purpose</StyledTableCell>
-            <StyledTableCell align="right">Product Type</StyledTableCell>
-            <StyledTableCell align="right">Manufacturer name</StyledTableCell>
-          </TableRow>
-          {error && <ErrorFallback error={error} />}
-        </TableHead>
+    <Paper>
+      <TableContainer>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Brand Name</StyledTableCell>
+              <StyledTableCell align="right">Purpose</StyledTableCell>
+              <StyledTableCell align="right">Product Type</StyledTableCell>
+              <StyledTableCell align="right">Manufacturer name</StyledTableCell>
+            </TableRow>
+            {error && <ErrorFallback error={error} />}
+          </TableHead>
 
-        <TableBody>
-          {drugs?.length > 0 &&
-            drugs.map((row: Row) => (
-              <StyledTableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <StyledTableCell component="th" scope="row">
-                  <StyledLink to={`/details/${row.product_ndc.toLowerCase()}`}>
-                    {row.brand_name}
-                  </StyledLink>
-                </StyledTableCell>
-                <StyledTableCell align="right">{row.purpose}</StyledTableCell>
-                <StyledTableCell align="right">
-                  {row.product_type}
-                </StyledTableCell>
-                <StyledTableCell align="right">
-                  {row.manufacturer_name}
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            {totalResults > 0 && (
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                colSpan={3}
-                count={totalResults}
-                rowsPerPage={rowsPerPage}
-                page={pageQuery - 1}
-                slotProps={{
-                  select: {
-                    inputProps: {
-                      "aria-label": "rows per page",
-                    },
-                    native: true,
-                  },
-                }}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationControls}
-              />
-            )}
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+          <TableBody>
+            {drugs?.length > 0 &&
+              drugs.map((row: Row) => (
+                <StyledTableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <StyledTableCell component="th" scope="row">
+                    <StyledLink
+                      to={`/details/${row.product_ndc.toLowerCase()}`}
+                    >
+                      {row.brand_name}
+                    </StyledLink>
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.purpose}</StyledTableCell>
+                  <StyledTableCell align="right">
+                    {row.product_type}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {row.manufacturer_name}
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {totalResults > 0 && (
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+          component="div"
+          count={totalResults}
+          rowsPerPage={rowsPerPage}
+          page={pageQuery - 1}
+          slotProps={{
+            select: {
+              inputProps: {
+                "aria-label": "rows per page",
+              },
+              native: true,
+            },
+          }}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          ActionsComponent={TablePaginationControls}
+        />
+      )}
+    </Paper>
   );
 };
 
